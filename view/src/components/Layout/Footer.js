531b6c74b74da3bd4c24223bcab9215d86640e38
Acesif <asifpalak@gmail.com>
2023-04-07 21:22:24 +0600

    made changes to view

--- view/src/components/Layout/Footer.js
+++ view/src/components/Layout/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      
+    <div style={{minHeight: '10vh'}} className='bg-dark text-light p-3'>
+      <h4 className='text-center'>All Rights Reserved &copy; DigiPharma</h4>
     </div>
   )
 }
 
 export default Footer
+
--- view/src/components/Layout/Layout.js
+++ view/src/components/Layout/Layout.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
-function Layout() {
+function Layout({children}) {
   return (
     <div>
-      
+      <Header/>
+      <main style={{minHeight: '80vh'}}>{children}</main>
+      <Footer/>
     </div>
   )
 }
