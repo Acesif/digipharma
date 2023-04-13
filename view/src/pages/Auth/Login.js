b0bf3b9352a9b3883a4427b2770ea2e4442f4b58
Acesif <asifpalak@gmail.com>
2023-04-14 00:50:04 +0600

    save last state

--- view/src/pages/Auth/Login.js
+++ view/src/pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout.js'
 import axios from "axios"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import {toast} from 'react-hot-toast'
 import { useAuth } from '../../context/auth.js'
 
@@ -11,6 +11,7 @@ const Login = () => {
     const [auth,setAuth] = useAuth()
 
     const navigate = useNavigate();
+    const location = useLocation()
 
     // handle form submission
     const handleSubmit = async(e) =>{
@@ -25,7 +26,7 @@ const Login = () => {
                     token: res.data.token,
                 })
                  localStorage.setItem("auth",JSON.stringify(res.data));
-                navigate('/');
+                navigate(location.state || '/');
             }
             else{
                 toast.error(res.data.message)
