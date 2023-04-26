738b4d6f130eb71fef0344412466cdc740fa8475
Acesif <asifpalak@gmail.com>
2023-04-26 18:38:32 +0600

    admin dashboard

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,29 +1,35 @@
 import express from "express";
-import {registerController, loginController, testController, forgotPasswordController} from '../controller/authController.js';
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controller/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
+
 //routing
 //REGISTER || METHOD POST
-router.post('/register',registerController)
+router.post("/register", registerController);
 
-// LOGIN || POST
-router.post('/login',loginController);
+//LOGIN || POST
+router.post("/login", loginController);
 
-// Forgot Password || POST
-router.post('/forgot-password', forgotPasswordController)
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
 
-// test routes
-router.get('/test',requireSignIn,isAdmin,testController);
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
-//protected router auth - user
-router.get('/user-auth',requireSignIn, (req,res) => {
-    res.status(200).send({ok: true});
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
 });
-//protected router auth - admin
-router.get('/admin-auth',requireSignIn,isAdmin, (req,res) => {
-    res.status(200).send({ok: true});
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
 });
 
-export default router
+export default router;
